6c0d22eb78f408ceb746e1a54a6d5d4ebcdd2b23
Sungwon Kim <54873618+Won-Seong@users.noreply.github.com>
2025-11-25 07:13:47 +0000

    eos-token to pad-token

--- dataset.py
+++ dataset.py
@@ -19,8 +19,7 @@ def get_tokenizer(model_name):
     if tokenizer.mask_token is None:
         tokenizer.add_special_tokens({'mask_token': '[MASK]'})
     
-    if tokenizer.pad_token is None:
-        tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.pad_token = tokenizer.eos_token
         
     return tokenizer
 

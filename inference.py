7ec608e19dd68718d6002937af70bdc1135316af
Sungwon Kim <54873618+Won-Seong@users.noreply.github.com>
2025-12-08 07:43:49 +0000

    remote

--- inference.py
+++ inference.py
@@ -1,12 +1,11 @@
 import torch
-import os
 
 from ko_mini_llada.utils.sampler import Sampler
 from transformers import AutoModel, AutoTokenizer
 
 def get_sampler(model_name: str, checkpoint_path=None, device=None):
     load_path = checkpoint_path if checkpoint_path else model_name
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     
     model = AutoModel.from_pretrained(
         load_path, 

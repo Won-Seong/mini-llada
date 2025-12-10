c3b94989250bf40b2272847fbe61287e4b4cbb0a
Sungwon Kim <54873618+Won-Seong@users.noreply.github.com>
2025-12-10 10:56:19 +0000

    rm tokenizer

--- train.py
+++ train.py
@@ -134,7 +134,6 @@ def main():
     # 5. Init Trainer
     trainer = Trainer(
         model=model,
-        tokenizer=tokenizer,
         args=training_args,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
